4f7989c9b912202be6fc39dcd5c6e24cbd6c31c1
Tarek4521 <tarekulislam167@gmail.com>
2022-01-12 21:54:25 +0600

    firstcommit

---
+++ resources/views/components/master.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<!--   03:20:39 GMT -->
+<x-head/>
+
+
+<body>
+
+    <!--header area start-->
+    <x-header/>
+    <!--header area end-->
+
+{{$slot}}
+
+    
+  
+	
+	
+    <!--footer area start-->
+    <x-footer/>
+    <!--footer area end-->
+<!-- JS
+============================================ -->
+
+
+
+<!-- Plugins JS -->
+<script src="assets/js/plugins.js"></script>
+
+<!-- Main JS -->
+<script src="assets/js/main.js"></script>
+
+
+
+</body>
+
+<!--   03:22:07 GMT -->
+</html>
